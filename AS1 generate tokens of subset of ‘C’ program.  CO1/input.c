894b8be6e76be0abf623916b11b0455d671c0cec
Taufiq Attar <112812620+ATTTAUFIQ@users.noreply.github.com>
2024-05-14 22:43:48 +0530

    Add files via upload

---
+++ AS1 generate tokens of subset of ‘C’ program.  CO1/input.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+extern int yylex(); // Declare yylex function defined in lex.yy.c
+void main()
+{
+    // addition 
+  printf("Accept number a");
+ /* number is 
+accepted */
+yylex();
+ }
